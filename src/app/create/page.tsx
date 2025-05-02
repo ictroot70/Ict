285a641215a02a17476da398d3a93b6a6c5245a7
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-05-02 23:45:20 +0300

    SCRUM-15 feat: add initial pages for authentication, user profile, and content management

---
+++ src/app/create/page.tsx
@@ -0,0 +1,7 @@
+export default function CreatePage() {
+  return (
+    <>
+      <h2>Create New Publication</h2>
+    </>
+  );
+}
---
+++ src/app/statistics/page.tsx
@@ -0,0 +1,7 @@
+export default function Statistics() {
+  return (
+    <>
+      <h2>Statistics Page</h2>
+    </>
+  );
+}
