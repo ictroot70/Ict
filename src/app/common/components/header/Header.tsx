58d6f25bc61e1fad738e1ed9c53aab8e7c63324b
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-05-03 11:40:17 +0300

    SCRUM-11: <customized routing>

--- src/app/common/components/header/Header.tsx
+++ src/app/common/components/header/Header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
     return (
         <>
             <Link href="/">Home</Link>
-            <Link href="/(auth)/sign-in">SingIn</Link>
-            <Link href="/(auth)/sign-up">SingUp</Link>
-            <Link href="/(auth)/privacy">Privacy</Link>
+            <Link href="/sign-in">SingIn</Link>
+            <Link href="/sign-up">SingUp</Link>
+            <Link href="/privacy">Privacy</Link>
         </>
     );
 }
