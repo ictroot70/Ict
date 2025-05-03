cf75977cdce0303296ccbb4c65048587f8b1d729
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-05-03 11:56:56 +0300

    SCRUM-11: <fixed Header>

--- src/app/common/components/header/Header.tsx
+++ src/app/common/components/header/Header.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <>
+        <header className={"head"}>
             <Link href="/">Home</Link>
             <Link href="/sign-in">SingIn</Link>
             <Link href="/sign-up">SingUp</Link>
             <Link href="/privacy">Privacy</Link>
-        </>
+        </header>
     );
 }
