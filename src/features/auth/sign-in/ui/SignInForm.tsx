9a2d2d1f13778f413f7ec769bb31ef21c4071caa
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-08 00:44:05 +0300

    SCRUM-48 feat: refactor SignInForm to use ControlledInput and improve form handling

--- src/features/auth/sign-in/ui/SignInForm.tsx
+++ src/features/auth/sign-in/ui/SignInForm.tsx
@@ -1,28 +1,50 @@
 'use client'
-import '@ictroot/ui-kit/style.css'
+import { Button, Card, Typography } from '@/shared'
 import { GitHub, Google } from '@ictroot/ui-kit'
-import { Card, Typography, Input, Button } from '@/shared'
 
+import { ControlledInput } from '@/features/formControls/input/ui'
+import { useForm } from 'react-hook-form'
 import s from './SignInForm.module.scss'
 
 export const SignInForm = () => {
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmitHandler = (data: any) => {
+    console.log(data)
+    reset()
+  }
+
   return (
     <Card className={s.wrapper}>
       <Typography variant="h1" className={s.title}>
         Sign In
       </Typography>
       <div className={s.oauthProviders}>
-        <Button as="a" variant="text">
+        <Button as="a" variant="text" href="#google">
           <Google size={36} />
         </Button>
-        <Button as="a" variant="text">
+        <Button as="a" variant="text" href="#github">
           <GitHub size={36} color="var(--color-light-100)" />
         </Button>
       </div>
-      <form action="" className={s.form} autoComplete="off">
+      <form className={s.form} autoComplete="off" onSubmit={handleSubmit(onSubmitHandler)}>
         <div className={s.fields}>
-          <Input id="email" inputType="text" label="Email" placeholder="Your email..." />
-          <Input
+          <ControlledInput
+            name="email"
+            control={control}
+            id="email"
+            inputType="text"
+            label="Email"
+            placeholder="Your email..."
+          />
+          <ControlledInput
+            name="password"
+            control={control}
             id="password"
             inputType="hide-able"
             label="Password"
@@ -31,8 +53,10 @@ export const SignInForm = () => {
           />
         </div>
 
-        <Typography variant="regular_14" asChild className={s.link}>
-          <a href="#">Forgot Password</a>
+        <Typography variant="regular_14" asChild>
+          <a href="#" className={s.link}>
+            Forgot Password
+          </a>
         </Typography>
 
         <Button variant="primary" fullWidth>
@@ -41,7 +65,7 @@ export const SignInForm = () => {
       </form>
       <div className={s.hasAccount}>
         <Typography variant="regular_16">Don’t have an account?</Typography>
-        <Button as="a" variant="text" fullWidth>
+        <Button as="a" variant="text" fullWidth href="#sign-up">
           Sign Up
         </Button>
       </div>
