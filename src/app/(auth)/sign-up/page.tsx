b7a0c7cd6934f0a08dde53c1e8b705ce279bed47
TimofeyOst <timofey.ostapchuk@gmail.com>
2025-06-08 21:53:23 +0300

    fix: rollback signup page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,14 +1,7 @@
-import {EmailConfirmed} from "@/features/auth/email-confirmed/ui/EmailConfirmed";
-import {EmailExpired} from "@/features/auth/email-expired/ui/EmailExpired";
-
 export default function SingUp() {
     return (
-        // <EmailConfirmed/>
-        <EmailExpired/>
-        // <main>
-        //     <h1>Hello SingUp!</h1>
-            // <EmailConfirmed/>
-        // </main>
-
+        <main>
+            <h1>Hello SingUp!</h1>
+        </main>
     )
 }
