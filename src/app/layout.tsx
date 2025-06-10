f6f3c02c8202f10a5468476ad8163758f13bd605
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-10 09:06:49 +0300

    SCRUM-48 Login form markup (#12)
    * SCRUM-49 feat: restructure styles and add new SCSS files for better organization
    
    * SCRUM-49 feat: add Header component with styles and update layout structure
    
    * SCRUM-48 feat: implement SignInForm component and styles
    
    * SCRUM 49 feat: add global styles and remove unused SCSS files for improved organization
    
    * SCRUM-49 feat: update .head styles and add main layout for improved structure
    
    * SCRUM-49 feat: remove unused Header.module.scss and update Header component to use global styles
    
    * SCRUM-49 feat: add id prop to InputProps and update OmitControllerFieldProps to exclude it
    
    * SCRUM-48 feat: refactor SignInForm to use ControlledInput and improve form handling
    
    * SCRUM-48 fix: remove unnecessary !important from link margin and ensure focus-visible outline is applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import "./globals.css";
-import {ReactNode} from 'react';
-import { Inter } from "next/font/google";
-import Header from '@/app/common/components/header/Header';
+import '@ictroot/ui-kit/style.css'
+import './globals.css'
+
+import { ReactNode } from 'react'
+import { Inter } from 'next/font/google'
+import Header from '@/app/common/components/header/Header'
 
 const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-family",
-    display: "swap",
-});
-
+  subsets: ['latin'],
+  variable: '--font-family',
+  display: 'swap',
+})
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: ReactNode;
+  children,
+}: Readonly<{
+  children: ReactNode
 }>) {
-    return (
-        <html lang="en">
-        <body className={`${inter.variable}`}>
-        <Header/>
+  return (
+    <html lang="en">
+      <body className={`${inter.variable}`}>
+        <Header />
         <main>{children}</main>
-
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  )
 }
