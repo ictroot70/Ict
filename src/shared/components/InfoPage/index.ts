49df8485a045b25fb695dcb05f28c1642e7b5c39
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-16 13:16:33 +0300

    SCRUM-57 feat: add privacy policy and terms of service text assets

---
+++ src/shared/components/InfoPage/index.ts
@@ -0,0 +1,5 @@
+import { InfoPage } from './ui/InfoPage'
+import { privacyPolicyText } from './assets/privacyPolicyText'
+import { termsOfServiceText } from './assets/termsOfServiceText'
+
+export { InfoPage, privacyPolicyText, termsOfServiceText }
