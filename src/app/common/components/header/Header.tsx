929086083475d37c1ee2535a878ef6228ff97476
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-18 12:08:58 +0300

    SCRUM-57 refactor: correct spelling in sign-in and sign-up links in Header component

--- src/app/common/components/header/Header.tsx
+++ src/app/common/components/header/Header.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-
+import Link from 'next/link'
 
 export default function Header() {
-    return (
-        <header className={"head"}>
-            <Link href="/">Home</Link>
-            <Link href="/sign-in">SingIn</Link>
-            <Link href="/sign-up">SingUp</Link>
-            <Link href="/privacy">Privacy</Link>
-        </header>
-    );
+  return (
+    <header className={'head'}>
+      <Link href="/">Home</Link>
+      <Link href="/sign-in">SingIn</Link>
+      <Link href="/sign-up">SingUp</Link>
+      <Link href="/privacy-policy">Privacy Policy</Link>
+      <Link href="/terms-of-service">Terms of Service</Link>
+    </header>
+  )
 }
