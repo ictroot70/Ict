b6dce93fcd0f54e7fa3e849ba8bf0afcb0b458a4
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-23 12:30:57 +0300

    SCRUM-68 add: create EmailConfirmed and EmailExpired pages

---
+++ src/app/(auth)/email-confirmed/page.tsx
@@ -0,0 +1,5 @@
+import { EmailConfirmed } from '@/features/auth/email-confirmed/ui/EmailConfirmed'
+
+export default function EmailConfirmedPage() {
+  return <EmailConfirmed />
+}
---
+++ src/app/(auth)/email-expired/page.tsx
@@ -0,0 +1,5 @@
+import { EmailExpired } from '@/features/auth/email-expired/ui/EmailExpired'
+
+export default function EmailExpiredPage() {
+  return <EmailExpired />
+}
