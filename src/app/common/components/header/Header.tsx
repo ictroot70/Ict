726e43a937833ef3d7d2a22e3dc6386c20658d90
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-23 12:31:35 +0300

    SCRUM-68 fix(Header): update links to include Forgot Password, Create New Password, Email Expired, and Email Confirmed

--- src/app/common/components/header/Header.tsx
+++ src/app/common/components/header/Header.tsx
@@ -6,7 +6,10 @@ export default function Header() {
       <Link href="/">Home</Link>
       <Link href="/sign-in">SingIn</Link>
       <Link href="/sign-up">SingUp</Link>
-      <Link href="/privacy">Privacy</Link>
+      <Link href="/forgot-password">Forgot Password</Link>
+      <Link href="/create-new-password">Create New Password</Link>
+      <Link href="/email-expired">Email Expired</Link>
+      <Link href="/email-confirmed">Email Confirmed</Link>
     </header>
   )
 }
