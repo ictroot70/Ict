75497800c5c3954364f0ff3b32f109411d87675f
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-06-29 23:16:40 +0300

    SCRUM-68 feat(ForgotPasswordForm): implement email resend functionality and update routes

--- src/features/auth/forgot-password/ui/ForgotPasswordForm.tsx
+++ src/features/auth/forgot-password/ui/ForgotPasswordForm.tsx
@@ -4,7 +4,7 @@ import s from './ForgotPasswordForm.module.scss'
 
 import { ControlledInput } from '@/features/formControls/input/ui'
 import { Button, Card, Recaptcha, Typography } from '@/shared'
-import ModalEmailSent from './ModalEmailSent/ModalEmailSent'
+import ModalEmailSent from '@/common/components/ModalEmailSent/ModalEmailSent'
 
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
@@ -14,10 +14,12 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { passwordRecovery } from '../api/passwordRecovery'
 import { useState } from 'react'
+import { ROUTES } from '../config/constants'
+import { passwordRecoveryResending } from '../../email-expired/ui/api/passwordRecoveryResending'
 
 const forgotPasswordSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email address'),
-  recaptcha: z.string().min(1, 'Please complete the reCAPTCHA'),
+  recaptcha: z.string().optional(),
 })
 
 type Inputs = z.infer<typeof forgotPasswordSchema>
@@ -25,14 +27,16 @@ type Inputs = z.infer<typeof forgotPasswordSchema>
 export default function ForgotPasswordForm() {
   const [isOpenModalWindow, setIsOpenModalWindow] = useState(false)
   const [currentEmail, setCurrentEmail] = useState('')
+  const [isEmailSent, setIsEmailSent] = useState(false)
 
   const {
     control,
     handleSubmit,
     reset,
+    watch,
     setValue,
     setError,
-    formState: { errors, isSubmitted, isValid },
+    formState: { isValid },
   } = useForm<Inputs>({
     defaultValues: { email: '', recaptcha: '' },
     resolver: zodResolver(forgotPasswordSchema),
@@ -40,25 +44,50 @@ export default function ForgotPasswordForm() {
   })
 
   const router = useRouter()
+  const recaptchaValue = watch('recaptcha')
 
   const onSubmit = async ({ email, recaptcha }: Inputs) => {
-    const response = await passwordRecovery({
-      email,
-      recaptcha,
-      baseUrl: window.location.origin,
-    })
-
-    if (response.ok) {
-      setCurrentEmail(email)
-      setIsOpenModalWindow(true)
-      reset()
+    if (isEmailSent) {
+      const response = await passwordRecoveryResending({
+        email,
+        baseUrl: window.location.origin + ROUTES.createNewPassword,
+      })
+      if (response.ok) {
+        setCurrentEmail(email)
+        setIsOpenModalWindow(true)
+      } else {
+        setError('email', { type: 'custom', message: "User with this email doesn't exist" })
+      }
     } else {
-      setError('email', { type: 'custom', message: "User with this email doesn't exist" })
+      if (!recaptcha) {
+        setError('recaptcha', { type: 'custom', message: 'Please complete the reCAPTCHA' })
+        return
+      }
+
+      const response = await passwordRecovery({
+        email,
+        recaptcha,
+        baseUrl: window.location.origin + ROUTES.createNewPassword,
+      })
+
+      if (response.ok) {
+        setCurrentEmail(email)
+        setIsOpenModalWindow(true)
+        setIsEmailSent(true)
+        reset()
+      } else {
+        setError('email', { type: 'custom', message: "User with this email doesn't exist" })
+      }
     }
   }
 
   const handleRecaptchaChange = async (token: string = '') => {
     setValue('recaptcha', token, { shouldValidate: true })
+  }
+
+  const handleCloseModalWindow = () => {
+    setIsOpenModalWindow(false)
+    setCurrentEmail('')
   }
 
   return (
@@ -76,28 +105,41 @@ export default function ForgotPasswordForm() {
               label="Email"
               placeholder="Enter your email"
             />
-
             <Typography variant={'regular_14'} className={s.text}>
               Enter your email address and we will send you further instructions
             </Typography>
 
-            <Button type="submit" fullWidth disabled={!isValid} className={s.button}>
-              Send Link
+            {isEmailSent && (
+              <Typography variant={'regular_14'} className={s.message}>
+                The link has been sent by email.
+                <br />
+                If you don’t receive an email send link again
+              </Typography>
+            )}
+
+            <Button
+              type="submit"
+              fullWidth
+              disabled={isEmailSent ? !isValid : !isValid || !recaptchaValue}
+              className={s.button}
+            >
+              {isEmailSent ? 'Send Link Again' : 'Send Link'}
             </Button>
 
             <Button
               variant={'text'}
               fullWidth
-              onClick={() => router.push('/sign-in')}
+              onClick={() => router.push(ROUTES.signIn)}
               className={s.button}
             >
               Back to Sign In
             </Button>
-
-            <Recaptcha
-              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-              onChange={handleRecaptchaChange}
-            />
+            {!isEmailSent && (
+              <Recaptcha
+                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                onChange={handleRecaptchaChange}
+              />
+            )}
           </form>
         </div>
       </Card>
@@ -105,7 +147,7 @@ export default function ForgotPasswordForm() {
       <ModalEmailSent
         email={currentEmail}
         isOpen={isOpenModalWindow}
-        onClose={() => setIsOpenModalWindow(false)}
+        onClose={handleCloseModalWindow}
       />
     </>
   )
