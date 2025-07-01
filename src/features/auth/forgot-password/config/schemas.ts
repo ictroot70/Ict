34b78aae253dd292d50a24581d943bafa9178060
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-07-01 14:34:18 +0300

    SCRUM-68 refactor(ForgotPasswordSchema): move ForgotPasswordInputs type definition below schema

--- src/features/auth/forgot-password/config/schemas.ts
+++ src/features/auth/forgot-password/config/schemas.ts
@@ -4,3 +4,5 @@ export const forgotPasswordSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email address'),
   recaptcha: z.string().optional(),
 })
+
+export type ForgotPasswordInputs = z.infer<typeof forgotPasswordSchema>
