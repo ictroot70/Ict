b74328605fccafe5aa25e040bc6ca80ceae87d4c
Sem Antonyan <sem.antony.dev@gmail.com>
2025-07-11 18:39:48 +0400

    update user information

--- src/app/users/profile/[id]/page.tsx
+++ src/app/users/profile/[id]/page.tsx
@@ -5,21 +5,24 @@ import { RequireAuth } from '@/features/auth/ui'
 
 export default function Profile() {
   const params = useParams()
-  const { data } = useGetMyProfileQuery()
+  const { data, isSuccess } = useGetMyProfileQuery()
   const userId = params?.id
   console.log('userId', userId)
   return (
     <RequireAuth>
-      <div>
-        <h1>My Profile</h1>
-        <p>My id is: {userId}</p>
-        <p>My userName is: {data?.userName}</p>
-        <p>My firstName is: {data?.firstName}</p>
-        <p>My lastName is: {data?.lastName}</p>
-        <p>My country is: {data?.country}</p>
-        <p>My city is: {data?.city}</p>
-        <p>My region is: {data?.region}</p>
-      </div>
+
+      {isSuccess &&  (
+        <div>
+          <h1>My Profile</h1>
+          <p>My id is: {userId}</p>
+          <p>My userName is: {data?.userName}</p>
+          <p>My firstName is: {data?.firstName}</p>
+          <p>My lastName is: {data?.lastName}</p>
+          <p>My country is: {data?.country}</p>
+          <p>My city is: {data?.city}</p>
+          <p>My region is: {data?.region}</p>
+        </div>
+      )}
     </RequireAuth>
   )
 }
