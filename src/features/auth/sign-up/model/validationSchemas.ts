3a2aa3b10c2d3c51311781ca3c8a992a33681cfb
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-17 11:44:44 +0300

    SCRUN-84 fix: prevent usernames consisting only of underscores or hyphens

--- src/features/auth/sign-up/model/validationSchemas.ts
+++ src/features/auth/sign-up/model/validationSchemas.ts
@@ -6,7 +6,10 @@ export const signUpSchema = z
       .string()
       .min(6, 'Minimum number of characters 6')
       .max(30, 'Maximum number of characters 30')
-      .regex(/^[a-zA-Z0-9_-]*$/, 'Username can contain only: 0-9, A-Z, a-z, _, -'),
+      .regex(/^[a-zA-Z0-9_-]*$/, 'Username can contain only: 0-9, A-Z, a-z, _, -')
+      .refine(val => /[a-zA-Z0-9]/.test(val), {
+        message: 'Username must contain at least one letter or number',
+      }),
 
     email: z.string().email('The email must match the format example@example.com'),
 
