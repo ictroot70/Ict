03e661745222bfdef691dab6525a36288457716e
Sem Antonyan <sem.antony.dev@gmail.com>
2025-07-28 20:32:31 +0400

    refactor(OAuthIcons): updated the usage of the component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,26 +1,5 @@
-'use client'
 import { SignInForm } from '@/features/auth/sign-in/ui/SignInForm'
-import { useSignIn } from '@/features/auth/sign-in/model/useSignIn'
 
 export default function SingIn() {
-  const {
-    form: { control, formState },
-    onSubmit,
-    isLoading,
-  } = useSignIn()
-
-  return (
-    <SignInForm
-      control={control}
-      formState={{
-        errors: formState.errors,
-        isDirty: formState.isDirty,
-        isValid: formState.isValid,
-      }}
-      onSubmit={onSubmit}
-      isLoading={isLoading}
-      onSignInGoogle={() => console.log('Google login')}
-      onSignInGithub={() => console.log('GitHub login')}
-    />
-  )
+  return <SignInForm />
 }
