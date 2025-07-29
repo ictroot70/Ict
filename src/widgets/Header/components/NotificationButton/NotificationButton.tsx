f0bea7ef255242301964a1c8b0788abbc51bcb62
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-07-29 21:50:21 +0300

    SCRUM-91 refactor: refactor Header

--- src/widgets/Header/components/NotificationButton/NotificationButton.tsx
+++ src/widgets/Header/components/NotificationButton/NotificationButton.tsx
@@ -1,7 +1,13 @@
 import { BellOutline } from '@/shared/ui'
+import s from '../../AppHeader.module.scss'
 
 export const NotificationButton = () => (
-  <button title="Notification" type="button" onClick={() => alert('notification')}>
+  <button
+    title="Notification"
+    type="button"
+    onClick={() => alert('notification')}
+    className={s.notificationBtn}
+  >
     <BellOutline size={24} />
   </button>
 )
--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Header_v2, Typography } from '@/shared/ui'
+import { Button, Header, Typography } from '@/shared/ui'
 import Link from 'next/link'
 import { useMeQuery } from '@/features/auth/api/authApi'
 import { useState } from 'react'
@@ -7,9 +7,9 @@ import { LogoutModal } from '@/widgets/Header/components/LogoutModal'
 import { useLogoutHandler } from '@/widgets/Header/hooks/useLogoutHandler'
 import { NotificationButton } from '@/widgets/Header/components/NotificationButton/NotificationButton'
 import { LanguageSelect } from '@/widgets/Header/components/LanguageSelect/LanguageSelect'
-import { AuthButtons } from '@/widgets/Header/components/AuthButtons/AuthButtons'
 import { useHomeLink } from '@/widgets/Header/hooks/useHomeLink'
-import styles from './AppHeader.module.scss'
+import s from './AppHeader.module.scss'
+import { APP_ROUTES } from '@/shared/constant/app-routes'
 export const AppHeader = () => {
   const [showLogoutModal, setShowLogoutModal] = useState(false)
   const { data: user, isLoading, isSuccess, isError } = useMeQuery()
@@ -22,32 +22,43 @@ export const AppHeader = () => {
   }
   return (
     // TODO: This is a temporary header(these buttons should be removed in the future)
-    <Header_v2
-      isAuthorized={isAuthorized}
-      className={styles.header}
-      logo={
-        <Link href={homeLink}>
-          <Typography variant={'h1'}>ICTRoot</Typography>
-        </Link>
-      }
-    >
-      {!isLoading && isAuthorized && (
-        <Button variant="primary" onClick={() => setShowLogoutModal(true)}>
-          Logout
-        </Button>
-      )}
+    <>
       <LogoutModal
         open={showLogoutModal}
         onConfirm={handleLogout}
         onClose={handleCancelLogout}
         userEmail={user?.email}
       />
-      <div className={styles.headerControls}>
-        {!isLoading && isAuthorized && <NotificationButton />}
 
-        <LanguageSelect />
-        {!isLoading && !isAuthorized && <AuthButtons />}
-      </div>
-    </Header_v2>
+      <Header isAuthorized={isAuthorized} className={s.header}>
+        <div className={s.container}>
+          <Link href={homeLink}>
+            <Typography variant={'h1'}>ICTRoot</Typography>
+          </Link>
+          {!isLoading && (
+            <div className={s.headerControls}>
+              {isAuthorized && <NotificationButton />}
+              <LanguageSelect />
+              <div className={s.authButtons}>
+                {isAuthorized ? (
+                  <Button variant="primary" onClick={() => setShowLogoutModal(true)}>
+                    Logout
+                  </Button>
+                ) : (
+                  <>
+                    <Button as={Link} href={APP_ROUTES.AUTH.LOGIN} variant={'text'}>
+                      Log in
+                    </Button>
+                    <Button as={Link} href={APP_ROUTES.AUTH.REGISTRATION}>
+                      Sign up
+                    </Button>
+                  </>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </Header>
+    </>
   )
 }
