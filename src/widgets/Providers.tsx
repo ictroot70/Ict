0bd25b17b449f728fe39476ac1c55e72fb7cd632
Sem Antonyan <sem.antony.dev@gmail.com>
2025-08-01 09:00:16 +0400

    refactor: formate and rename

--- src/widgets/Providers.tsx
+++ src/widgets/Providers.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { ToastProvider } from '@ictroot/ui-kit'
 import { ReactNode } from 'react'
 
-export function AppProviders({ children }: { children: ReactNode }) {
+import { ToastProvider } from '@ictroot/ui-kit'
+
+export function ClientToastProvider({ children }: { children: ReactNode }) {
   return (
-    <ToastProvider position="bottom-right" enableProgressBar={true} maxToasts={3} enableHoverPause>
+    <ToastProvider position={'bottom-right'} enableProgressBar maxToasts={3} enableHoverPause>
       {children}
     </ToastProvider>
   )
