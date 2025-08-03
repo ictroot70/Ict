ad4914a5d9f26842254bfd0b31a9a657a4696616
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-08-03 03:27:22 +0300

    SCRUM-91 refactor: refactor AgreementLabel

--- src/features/auth/sign-up/ui/AgreementLabel.tsx
+++ src/features/auth/sign-up/ui/AgreementLabel.tsx
@@ -4,20 +4,8 @@ import Link from 'next/link'
 export const AgreementLabel = (
   <>
     I agree to the&nbsp;
-    <Link
-      href={APP_ROUTES.LEGAL.TERMS}
-      rel={'noopener noreferrer'}
-      onClick={e => e.stopPropagation()}
-    >
-      Terms of Service
-    </Link>
+    <Link href={APP_ROUTES.LEGAL.TERMS}>Terms of Service</Link>
     &nbsp;and&nbsp;
-    <Link
-      href={APP_ROUTES.LEGAL.PRIVACY}
-      rel={'noopener noreferrer'}
-      onClick={e => e.stopPropagation()}
-    >
-      Privacy Policy
-    </Link>
+    <Link href={APP_ROUTES.LEGAL.PRIVACY}>Privacy Policy</Link>
   </>
 )
