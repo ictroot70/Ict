fbb1ff1271d99b98c4f847c04676ecd958be30c9
Sem Antonyan <sem.antony.dev@gmail.com>
2025-08-06 22:52:54 +0400

    chore: add special comment to trigger redeploy (no functional changes)

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -21,6 +21,7 @@ export const AppHeader = (): ReactElement => {
   const confirmLogout = () => handleCancelLogout()
 
   if (isError) {
+    // TODO: Add error handling later
     console.log('Failed to fetch user', isError)
   }
 
