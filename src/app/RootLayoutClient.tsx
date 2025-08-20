e5459a3b44d21e4b06897b065753674076b89e2f
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-08-20 10:07:06 +0300

    SCRUM-70 feat: implementing authorization and registration with GitHub

--- src/app/RootLayoutClient.tsx
+++ src/app/RootLayoutClient.tsx
@@ -5,9 +5,11 @@ import { ReactNode } from 'react'
 import { useGoogleAuth } from '@/shared/lib/hooks/useGoogleAuth'
 
 import styles from './RootLayoutClient.module.scss'
+import { useGitHubAuth } from '@/shared/lib/hooks/useGitHubAuth'
 
 export const RootLayoutClient = ({ children }: { children: ReactNode }) => {
   useGoogleAuth()
+  useGitHubAuth()
 
   return <main className={styles.main}>{children}</main>
 }
