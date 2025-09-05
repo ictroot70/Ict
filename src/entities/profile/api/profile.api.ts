84f71b658bfe2dc9f5d7152292ceeb7ea16dd25d
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-09-05 11:18:37 +0300

    SCRUM-98 feat: add public profile and profile with posts API queries; update ProfileClient component

--- src/entities/profile/api/profile.api.ts
+++ src/entities/profile/api/profile.api.ts
@@ -1,4 +1,10 @@
-import { ProfileUpdateDto, ProfileViewModel } from '@/entities/profile/api/api.types'
+import {
+  ProfileUpdateDto,
+  ProfileViewModel,
+  ProfileWithPostsResponse,
+  PublicProfileRequest,
+  PublicProfileResponse,
+} from '@/entities/profile/api/api.types'
 import { API_ROUTES } from '@/shared/api/api-routes'
 import { baseQueryWithReauth } from '@/shared/api/base-query.api'
 import { createApi } from '@reduxjs/toolkit/query/react'
@@ -19,7 +25,20 @@ export const profileApi = createApi({
         body,
       }),
     }),
+    getPublicProfile: builder.query<PublicProfileResponse, PublicProfileRequest>({
+      query: ({ profileId }) => ({
+        url: API_ROUTES.PUBLIC_USER.PROFILE(profileId),
+      }),
+    }),
+    getProfileWithPosts: builder.query<ProfileWithPostsResponse, string>({
+      query: userName => ({ url: `v1/users/${userName}` }),
+    }),
   }),
 })
 
-export const { useGetMyProfileQuery, useLazyGetMyProfileQuery } = profileApi
+export const {
+  useGetMyProfileQuery,
+  useLazyGetMyProfileQuery,
+  useGetPublicProfileQuery,
+  useGetProfileWithPostsQuery,
+} = profileApi
--- src/entities/profile/ui/ProfileClient.tsx
+++ src/entities/profile/ui/ProfileClient.tsx
@@ -2,94 +2,68 @@
 
 import { ReactElement } from 'react'
 
-import { useGetMyProfileQuery } from '@/entities/profile'
+import { useGetMyProfileQuery, useGetProfileWithPostsQuery } from '@/entities/profile'
 
 import s from './ProfileClient.module.scss'
 import Image from 'next/image'
-import { Button, Typography } from '@ictroot/ui-kit'
-import Link from 'next/link'
-import { Avatar } from '@/shared/composites/Avatar'
+import { Button, Typography } from '@/shared/ui'
+import { Avatar } from '@/shared/composites'
 
 export const ProfileClient = (): ReactElement => {
-  const { data, isSuccess } = useGetMyProfileQuery()
+  const { data: meInfo, isSuccess } = useGetMyProfileQuery()
+
+  const { data: profileWithPosts } = useGetProfileWithPostsQuery(meInfo?.userName as string, {
+    skip: !meInfo?.userName || !isSuccess,
+  })
+
+  const avatarUrl = profileWithPosts?.avatars[0]?.url
+
+  const statsData = [
+    { label: 'Following', value: profileWithPosts?.followingCount || 0 },
+    { label: 'Followers', value: profileWithPosts?.followersCount || 0 },
+    { label: 'Publications', value: profileWithPosts?.publicationsCount || 0 },
+  ]
 
   return (
     <>
       {isSuccess && (
         <div className={s.profile}>
           <div className={s.profileDetails}>
-            <Avatar className={s.profileAvatar} />
+            <Avatar size={204} image={avatarUrl} />
             <div className={s.profileInfo}>
               <div className={s.profileInfoHeader}>
-                <Typography variant="h1">UserName</Typography>
+                <Typography variant="h1">{meInfo.userName}</Typography>
                 <div className={s.profileActions}>
                   <Button variant="secondary">Profile Settings</Button>
                 </div>
               </div>
-              <div className={s.profileStats}>
-                <div className={s.profileStatsItem}>
-                  <Typography variant="bold_14">2 218</Typography>
-                  <Typography variant="regular_14">Following</Typography>
-                </div>
-                <div className={s.profileStatsItem}>
-                  <Typography variant="bold_14">2 358</Typography>
-                  <Typography variant="regular_14">Followers</Typography>
-                </div>
-                <div className={s.profileStatsItem}>
-                  <Typography variant="bold_14">2 764</Typography>
-                  <Typography variant="regular_14">Publications</Typography>
-                </div>
-              </div>
+              <ul className={s.profileStats}>
+                {statsData.map(({ label, value }, index) => (
+                  <li key={index} className={s.profileStatsItem}>
+                    <Typography variant="bold_14">{value}</Typography>
+                    <Typography variant="regular_14">{label}</Typography>
+                  </li>
+                ))}
+              </ul>
               <Typography variant="regular_16" className={s.profileAbout}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco{' '}
-                <Typography asChild variant="regular_link">
-                  <Link href="#link" target="_blank">
-                    laboris nisi ut aliquip ex ea commodo consequat.
-                  </Link>
-                </Typography>
+                {meInfo.aboutMe || 'No information has been added yet.'}
               </Typography>
             </div>
           </div>
-          <div className={s.profilePosts}>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_2.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-            <div className={s.profilePostsItem}>
-              <Image src={'/mock/image_3.jpg'} fill alt="Post_1" className={s.profileImage} />
-            </div>
-          </div>
+          <ul className={s.profilePosts}>
+            {Array.from({ length: 4 }).map((_, index) => {
+              return (
+                <div key={index} className={s.profilePostsItem}>
+                  <Image
+                    src={`/mock/image_${index + 1}.jpg`}
+                    fill
+                    alt={`Post_${index + 1}`}
+                    className={s.profileImage}
+                  />
+                </div>
+              )
+            })}
+          </ul>
         </div>
       )}
     </>
