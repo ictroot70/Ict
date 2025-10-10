059168c6954fb243b064ee79a9043e49910038ac
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-10-10 18:01:27 +0300

    SCRUM-104 refactor(profile): remove old profile and layout components

--- src/app/old_RootLayoutClient.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ReactNode } from 'react'
-
-import styles from './RootLayoutClient.module.scss'
-
-export const RootLayoutClient = ({ children }: { children: ReactNode }) => {
-  return <main className={styles.main}>{children}</main>
-}
