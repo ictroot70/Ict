49e597f0b28a0c4c2d82a072bd6a2a17532d4514
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-10-13 22:51:11 +0300

    SCRUM-104 feat(public): fetch and display public posts data in the Public component

--- src/entities/users/ui/public/index.tsx
+++ src/entities/users/ui/public/index.tsx
@@ -3,44 +3,59 @@
 
 import s from './Public.module.scss'
 
-import { useGetPublicPostsQuery } from '@/entities/users/api'
+import { publicUsersApi, useGetPublicPostsQuery } from '@/entities/users/api'
 import { Loading } from '@/shared/composites'
 
 import { PublicPost } from './PublicPost/PublicPost'
 import { UsersCounter } from './UsersCounter/UsersCounter'
-import { mockDataPosts } from '../../model/mockDataForPublicPosts'
-import { useMeQuery } from '@/features/auth'
-import { APP_ROUTES } from '@/shared/constant'
 
-export function Public() {
-  const { data, isLoading, isError } = useGetPublicPostsQuery({ pageSize: 16 })
-  const { data: isAuth } = useMeQuery()
+import { APP_ROUTES } from '@/shared/constant'
+import { GetPublicPostsResponse, PublicPostResponse } from '../../api/api.types'
+import { useEffect, useRef } from 'react'
+import { useAppStore } from '@/lib/hooks'
+
+type Props = {
+  postsData: GetPublicPostsResponse
+}
+
+export function Public({ postsData }: Props) {
+  const needInitPostsInStore = useRef(!!postsData)
+  const store = useAppStore()
+
+  const { data, isLoading, isError } = useGetPublicPostsQuery(
+    { pageSize: 4 },
+    { skip: needInitPostsInStore.current }
+  )
+
+  useEffect(() => {
+    if (needInitPostsInStore.current) {
+      store.dispatch(
+        publicUsersApi.util.upsertQueryData('getPublicPosts', { pageSize: 4 }, postsData)
+      )
+      needInitPostsInStore.current = false
+    }
+  }, [])
+
+  const dataForRender = data || postsData
 
   if (isLoading) {
     return <Loading />
   }
+
   if (isError) {
     return <div>Something went wrong</div>
   }
-  if (!data) {
+
+  if (!dataForRender) {
     return null
   }
 
-  const { items, totalUsers } = data
+  const { items, totalUsers } = dataForRender
 
   return (
     <div className={s.container}>
       <UsersCounter totalCount={totalUsers || 0} />
       <div className={s.posts}>
-        {mockDataPosts.map((item, index) => {
-          return (
-            <PublicPost
-              key={index}
-              post={item}
-              urlProfile={APP_ROUTES.PROFILE.ID(`${item.ownerId}`)}
-            />
-          )
-        })}
         {items.map(post => {
           return (
             <PublicPost
@@ -55,6 +70,17 @@ export function Public() {
   )
 }
 
-/* 
+//import { mockDataPosts } from '../../model/mockDataForPublicPosts'
+//import { useMeQuery } from '@/features/auth'
 
-*/
+// const { data: isAuth } = useMeQuery()
+
+/*  {mockDataPosts.map((item, index) => {
+          return (
+            <PublicPost
+              key={index}
+              post={item}
+              urlProfile={APP_ROUTES.PROFILE.ID(`${item.ownerId}`)}
+            />
+          )
+        })} */
