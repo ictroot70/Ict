8ce9bb557a9fa6cc743b7dfe584537dbbc2b845d
Sem Antonyan <sem.antony.dev@gmail.com>
2025-11-29 23:58:40 +0400

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
   images: {
@@ -7,13 +8,14 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
-        port: '',
+        port: undefined,
         pathname: '/trainee-instagram-api/**',
       },
     ],
   },
-
-  /* config options here */
+  /* OTHER OPTIONS NEXT JS */
 }
 
-export default nextConfig
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
