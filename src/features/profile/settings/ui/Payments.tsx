1e9aa8eb2242a366c18911522dc81acec9039a76
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2026-03-12 19:13:56 +0300

    SCRUM-238 feat: add Payments table component with styling

--- src/features/profile/settings/ui/Payments.tsx
+++ src/features/profile/settings/ui/Payments.tsx
@@ -1,3 +1,40 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/shared/composites/Table'
+
 export function Payments() {
-  return <div>Payments for user </div>
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeader>Date of Payment</TableHeader>
+          <TableHeader>End date of subscription</TableHeader>
+          <TableHeader>Price</TableHeader>
+          <TableHeader>Subscription Type</TableHeader>
+          <TableHeader>My payments</TableHeader>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>12.03.2026</TableCell>
+          <TableCell>12.03.2026</TableCell>
+          <TableCell>$10</TableCell>
+          <TableCell>1 day</TableCell>
+          <TableCell>Stripe</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>12.03.2026</TableCell>
+          <TableCell>20.03.2026</TableCell>
+          <TableCell>$50</TableCell>
+          <TableCell>7 days</TableCell>
+          <TableCell>PayPal</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
 }
