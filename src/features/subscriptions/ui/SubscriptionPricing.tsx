ae02eed6d5e61b7e99b438f073f691a79b8d3234
Smbat1505 <smbat1584@gmail.com>
2026-04-09 17:04:09 +0400

    SCRUM-209: fix React import for SubscriptionPricing tests

--- src/features/subscriptions/ui/SubscriptionPricing.tsx
+++ src/features/subscriptions/ui/SubscriptionPricing.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 import { useCurrentSubscriptionChain } from '@/features/subscriptions/hooks'
 import { Loading } from '@/shared/composites'
