b969338730043defe06aceb9151eed6ac6c92277
Dmitry Muzalev <muzalev771@gmail.com>
2026-07-13 15:36:10 +0300

    SCRUM-304 feat(messenger): implement upsertMessageInHistory function and related tests

--- src/entities/messenger/lib/index.ts
+++ src/entities/messenger/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './map-message-to-dialogue-preview'
 export * from './normalize-messenger-error'
 export * from './validate-incoming-message'
+export * from './upsert-message-in-history'
