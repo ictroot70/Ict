e2854153eedcccdf4c3f0d7ee643c494db33ef24
Smbat1505 <smbat1584@gmail.com>
2026-07-16 12:01:43 +0400

    fix: guard guest auth routes

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -2,11 +2,14 @@ import { Suspense } from 'react'
 
 import SignInFormContainer from '@/app/(public)/auth/login/SignInFormContainer'
 import { Loading } from '@/shared/composites/Loading'
+import { GuestGuard } from '@/shared/guards'
 
 export default function SingIn() {
   return (
     <Suspense fallback={<Loading />}>
-      <SignInFormContainer />
+      <GuestGuard>
+        <SignInFormContainer />
+      </GuestGuard>
     </Suspense>
   )
 }
